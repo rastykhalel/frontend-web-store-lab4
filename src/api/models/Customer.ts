953504633d45d0685f7cf46dc92cbe13bb0c9811
Kory Herzinger <kory.herzinger@gmail.com>
2023-05-15 14:05:19 -0700

    Added comments to Customer model

--- src/api/models/Customer.ts
+++ src/api/models/Customer.ts
@@ -2,7 +2,12 @@
     Represents a customer of the web store.
 */
 export type Customer = {
+    // a unique identifier representing this customer
     customerId: string;
+
+    // the customer's full name
     name: string;
+
+    // the customer's shipping address
     address: string;
 }
